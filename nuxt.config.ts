2eba7f411b4b90f9405710860aea1c848da7b107
Peggy2344 <peggy102104@gmail.com>
2025-01-24 14:14:55 +0800

    chore: configure SVG loader and add motion and icon modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,36 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import svgLoader from 'vite-svg-loader'
+
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', '@nuxt/image'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxtjs/tailwindcss',
+    '@nuxt/image',
+    '@vueuse/motion/nuxt',
+    '@nuxt/icon',
+    '@vueuse/nuxt',
+  ],
   devtools: { enabled: true },
   css: ['~/assets/main.css'],
   compatibilityDate: '2024-11-01',
+  vite: {
+    plugins: [
+      svgLoader({
+        svgoConfig: {
+          plugins: [
+            {
+              name: 'preset-default',
+              params: {
+                overrides: {
+                  removeViewBox: false,
+                },
+              },
+            },
+          ],
+        },
+      }),
+    ],
+  },
   eslint: {
     config: {
       stylistic: true,
