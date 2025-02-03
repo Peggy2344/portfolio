2e4faaf3944a9b513a4d830c94e8629b1d5368bf
Peggy2344 <peggy102104@gmail.com>
2025-02-03 16:14:44 +0800

    feat: wrap sections with ClientOnly to prevent SSR rendering issues

--- components/section/Project.vue
+++ components/section/Project.vue
@@ -1,12 +1,14 @@
 <template>
-  <GrainCard class-name="bg-lightBlack pt-[96px] relative overflow-visible pt-10 pb-[100px] px-6 md:p-10">
-    <div class="md:sticky md:top-[10px] lg:top-0 top-[20px] h-fit">
-      <h1 class="mt-10 md:mt-0 text-[50px] lg:text-[80px] font-bold text-center text-primary mb-5">
-        Projects
-      </h1>
-    </div>
-    <div class="flex justify-center">
-      <ContentProjectDetail />
-    </div>
-  </GrainCard>
+  <ClientOnly>
+    <GrainCard class-name="bg-lightBlack pt-[96px] relative overflow-visible pt-10 pb-[100px] px-6 md:p-10">
+      <div class="md:sticky md:top-[10px] lg:top-0 top-[20px] h-fit">
+        <h1 class="mt-10 md:mt-0 text-[50px] lg:text-[80px] font-bold text-center text-primary mb-5">
+          Projects
+        </h1>
+      </div>
+      <div class="flex justify-center">
+        <ContentProjectDetail />
+      </div>
+    </GrainCard>
+  </ClientOnly>
 </template>
